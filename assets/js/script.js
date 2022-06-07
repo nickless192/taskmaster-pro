94634aff56a2fe0255a516acb4f1bb04a76a69df
Omar Rodriguez <omar.rguez26@gmail.com>
2022-06-07 14:15:43 -0400

    implemented moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -97,12 +99,20 @@ $(".list-group").on("click", "span", function() {
   // swap out element
   $(this).replaceWith(dateInput);
 
+  //enable datepick
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  })
+
   //automatially focus on new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this).val().trim();
 
@@ -122,6 +132,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
   //replace input with span element
   $(this).replaceWith(taskSpan);
 
+  auditTask($(taskSpan).closest(".list-group-item"));
 
 })
 
@@ -237,6 +248,29 @@ $("#trash").droppable({
     // console.log("out");
   }
 });
+
+// add datepicker to date input field
+$("#modalDueDate").datepicker( {
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert the moment object to 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is newar/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+}
 
 // load tasks for the first time
 loadTasks();
